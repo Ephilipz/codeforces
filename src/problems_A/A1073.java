6dd1fe1a5c2d24ce3feaea968cd8ee70ef5dd534
Eesaa Philips <32400503+ephilipz@users.noreply.github.com>
2018-11-24 09:54:14 +0200

    original commit

--- src/problems_A/A1073.java
+++ src/problems_A/A1073.java
@@ -7,7 +7,6 @@ public class A1073 {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         String s = in.next();
-        String res = "";
         for (int i = 0; i < n - 1; i++) {
             if (s.charAt(i) != s.charAt(i + 1)) {
                 System.out.println("YES\n" + s.charAt(i) + s.charAt(i + 1));
